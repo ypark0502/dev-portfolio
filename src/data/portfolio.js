663b7d04188cfbf20c9a63ae09524cc278f26cfc
Park, Ewan <ypark0502@gmail.com>
2023-04-28 21:33:41 -0400

    fixed portfolio links

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,18 +3,18 @@ export default [
         title: "AudioT Corporate Website",
         imgUrl: '/assets/audiot.jpg',
         stack: ['React', 'Next.js', 'FaunaDB'],
-        link: 'audiot.vercel.app'
+        link: 'https://audiot.vercel.app'
     },
     {
         title: "Developer Portfolio",
         imgUrl: '/assets/devportfolio.jpg',
         stack: ['React', 'Tailwind', 'Vite'],
-        link: 'ewanpark.vercel.app'
+        link: 'https://ewanpark.vercel.app'
     },
     {
         title: "GTLI Website",
         imgUrl: '/assets/gtli.png',
         stack: ['HTML', 'CSS', 'Drupal'],
-        link: 'esl.gatech.edu'
+        link: 'https://esl.gatech.edu'
     },
 ]
