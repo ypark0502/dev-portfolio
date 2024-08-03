6427171fb6af498b2ea45d627574b127202e61b9
Park, Ewan <ypark0502@gmail.com>
2024-08-02 23:28:18 -0400

    update to correct name

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Ewan Park</h1>
+            <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Younghwan "Ewan" Park</h1>
             <p className="text-base md:text-xl mb-3 font-medium">CS Student, Web Developer, and Software Engineer</p>
             <p className="text-sm max-w-xl md:text-xl mb-6 font-bold">I am a third-year Computer Science student at the Georgia Institute of Technology! My main skills are in design and full-stack development, hopefully for social good!</p>
         </div>
